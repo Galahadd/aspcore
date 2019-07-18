d565a2708d529411baee17c8afb78e85069eb486
Recep Kaya <recep.kayaa@outlook.com>
2019-07-18 17:20:20 +0300

    Add project files.

---
+++ Abc.Northwind.DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic; 
+using System.Text;
+
+namespace Abc.Northwind.DataAccess.Concrete.EntityFramework
+{
+        public class EfCategoryDal : EfEntitiyRepositoryBase<Category, NortwindContext>, ICategoryDal
+        {
+
+        }
+    
+}
---
+++ Abc.Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,16 @@
+﻿using Abc.Core.EntityFramework;
+using Abc.Northwind.DataAccess.Abstract;
+using Abc.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abc.Northwind.DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : EfEntitiyRepositoryBase<Product, NortwindContext>, IProductDal
+    {
+
+    }
+    
+
+}
